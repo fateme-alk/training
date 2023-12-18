d1ca09b55712303a0f65b53ab3e74b310992fb0d
fateme-alk <fateme.alk9@gmail.com>
2023-12-18 16:53:56 +0330

    map blog rout to blog view

--- training/urls.py
+++ training/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("classes/", views.classes, name="classes"),
+    path("blog/", views.blog, name="blog"),
 ]
