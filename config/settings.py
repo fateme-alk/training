9e47e7e2fcd0fe50124be84bd6282ac81b3372d2
fateme-alk <fateme.alk9@gmail.com>
2024-01-09 15:52:23 +0330

    add media configuration

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,7 @@ STATICFILES_DIRS = [BASE_DIR / "config/static/"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = BASE_DIR / "media/"
