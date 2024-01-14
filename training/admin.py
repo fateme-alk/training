56abd8f9e2d6e7048674320ffe63c899cb13c059
fateme-alk <fateme.alk9@gmail.com>
2024-01-14 23:08:20 +0330

    implement TrainingCourse model and add relation with Participant model

--- training/admin.py
+++ training/admin.py
@@ -1,7 +1,8 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Participant, Trainer
+from .models import Participant, Trainer, TrainingCourse
 
 admin.site.register(Trainer)
 admin.site.register(Participant)
+admin.site.register(TrainingCourse)
--- training/models.py
+++ training/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.contrib.auth import get_user_model
 from django.db import models
+from multiselectfield import MultiSelectField  # type: ignore
 
 User = get_user_model()
 
@@ -27,8 +28,33 @@ class Trainer(models.Model):
             return self.user.username.title()
 
 
+class TrainingCourse(models.Model):
+    DAYS_OF_WEEK_CHOICES = (
+        ("mon", "Monday"),
+        ("tue", "Tuesday"),
+        ("wed", "Wednesday"),
+        ("thu", "Thursday"),
+        ("fri", "Friday"),
+        ("sat", "Saturday"),
+        ("sun", "Sunday"),
+    )
+
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    total_rate = models.PositiveIntegerField()
+    class_fee = models.FloatField()
+    remined_capacity = models.PositiveIntegerField()
+    class_time = models.TimeField()
+    days_of_week = MultiSelectField(max_length=10, choices=DAYS_OF_WEEK_CHOICES)
+
+    def __str__(self):
+        return self.title
+
+
 class Participant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    traing_classes = models.ManyToManyField(TrainingCourse)
 
     def __str__(self):
         first_name = self.user.first_name.title()
